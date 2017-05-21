8b0bc24797a68464a9bbd33850baa3efb8cd81e7
lmw6412036 <lmw6412036@sina.com>
2017-05-21 14:18:51 +0800

    增加模板引擎

--- app/controller/home.js
+++ app/controller/home.js
@@ -1,10 +1,10 @@
 'use strict';
 
 module.exports = app => {
-  class HomeController extends app.Controller {
-    * index() {
-      this.ctx.body = 'hi, egg';
+    class HomeController extends app.Controller {
+        * index() {
+            yield this.ctx.render('home/index', {name: "hi,egg"});
+        }
     }
-  }
-  return HomeController;
+    return HomeController;
 };
--- config/config.default.js
+++ config/config.default.js
@@ -1,12 +1,20 @@
 'use strict';
 
 module.exports = appInfo => {
-  const config = {};
+    const config = {};
 
-  // should change to your own
-  config.keys = appInfo.name + '_1495340017705_8146';
+    // should change to your own
+    config.keys = appInfo.name + '_1495340017705_8146';
 
-  // add your config here
+    // add your config here
+    //view
+    config.view={
+        mapping:{
+            ".html":"nunjucks",
+        },
+        defaultViewEngine:"nunjucks",
+        defaultExtension:".html"
+    };
 
-  return config;
+    return config;
 };
