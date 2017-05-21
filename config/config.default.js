6b8d38761514a71cb11040c5858e7da5bcb1d8bd
lmw6412036 <lmw6412036@sina.com>
2017-05-21 12:17:11 +0800

    创建项目

---
+++ config/config.default.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = {};
+
+  // should change to your own
+  config.keys = appInfo.name + '_1495340017705_8146';
+
+  // add your config here
+
+  return config;
+};
